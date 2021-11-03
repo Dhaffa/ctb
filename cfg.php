7ca41d3e49ac8de734551a21b10b95bbc139ea78
Dhaffa <hendisuhendi858@gmail.com>
2021-11-04 05:19:21 +0700

    Add files via upload

---
+++ cfg.php
@@ -0,0 +1,27 @@
+<?php
+
+/* data mining */
+
+$sign = "hmAd9iJULLZSl7RU8gFpws3mzk92Btjp1/q+DUi7J8tpbIbPLEJkGUO1U0Eo5n2K8mOAe9BZVtEkXiI2";
+$auth = "Bearer gAAAAABhgwFGUIWVdfHUjPE_lRWLaRoLDjmCf65VBpX2VYyOrV5AgXbfj6JAfQidom5rCgIMggv7nznKFB_YojuCJxwBuTrFrm5gTWfFLTRAJiek0GDsh_s=";
+
+/* data email */
+
+$sign1 = "zdpFzN3FU1icrGvbfCOdwf8m8svP6MXzFI1hym44BA+PHdcyguMrRIOooYdBoKNAf8cKLPnCQvhk2uL/";
+$auth1 = "Bearer gAAAAABhgwHUYrAzoSxA4CDNN2y5pAqEKykaj3XHEfqmPXt_ZJnvLxnajeFobzmBkylOopnCsrIA2uaXjACVrtqGEm8CEt-YnSVTnZ5_nB5C6zRjvpzacl4=";
+
+
+/* cookie */
+
+$user_agent = "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36";
+$ct_ts = "1635975636";
+$token = "csrftoken=9ihYTXzejydKgQ76wNH8dLawL2UmIGfF6viC3ALIaXLIBtYC6KJVgrdYTIIaqiHk";
+$session = "t3pdd8cpcz0olywohtywy4z6v15ov1al";
+$bid = "bid=0BD9D7A6-48B7-41DD-8148-8E688C4F6C39";
+$ct_v = "v=2.3.2";
+$ct_av = "av=2.0.0.359";
+
+
+
+?>
+
